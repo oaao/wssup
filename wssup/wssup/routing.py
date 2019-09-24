b16f0923194c666e07f041a6433d991d00f5f7d7
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-09-24 13:59:24 -0600

    add [chat]: url-route the websocket Consumer

--- wssup/wssup/routing.py
+++ wssup/wssup/routing.py
@@ -1,5 +1,11 @@
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 
+import chat.routing
+
 application = ProtocolTypeRouter(
-    {}  # http->django views is added by default
+    {
+        # 'http': channels.http.AsgiHandler (django views) added by default
+        'websocket': AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+    }
 )
