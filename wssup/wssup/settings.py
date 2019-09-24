30eb45169ba6e0ea929b2126260d35ede7d46132
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-09-24 13:11:58 -0600

    add [django]: replace django runserver with channels server

--- wssup/wssup/settings.py
+++ wssup/wssup/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
 
     'chat',
 
+    'channels'
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# together with 'channels' in INSTALLED_APPS
+# reassigns runserver command from django devt server to channels devt server
+ASGI_APPLICATION = 'wssup.routing.application'
