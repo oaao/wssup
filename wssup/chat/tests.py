68f16a2441122f26ea953035fa4d0f5e9b1108a1
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-09-25 15:20:35 -0600

    rev [chat]: tests - implement driver helper functions

--- wssup/chat/tests.py
+++ wssup/chat/tests.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from channels.testing import ChannelsLiveServerTestCase
 
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class ChatTests(ChannelsLiveServerTestCase):
@@ -22,3 +24,40 @@ class ChatTests(ChannelsLiveServerTestCase):
     def tearDownClass(cls):
         cls.driver.quit()
         super().tearDownClass()
+
+    # ---- TESTS ----
+
+
+    # ---- HELPERS ----
+    def _enter_room(self, name):
+
+        self.driver.get(self.live_server_url + '/chat/')
+
+        ActionChains(self.driver).send_keys(name + '\n').perform()
+
+        WebDriverWait(self.driver, 2).until(
+            lambda _ : name in self.driver.current_url
+        )
+
+    def _open_new_window(self):
+        self.driver.execute_script('window.open("about:blank", "_blank");')
+        self.driver.switch_to_window(self.driver.window_handles[-1])
+
+    def _close_all_new_windows(self):
+
+        while len(self.driver.window_handles) > 1:
+            self.driver.switch_to_window(self.driver.window_handles[-1])
+            self.driver.execute_script('window.close();')
+
+        if len(self.driver.window_handles) == 1:
+            self.driver.switch_to_window(self.driver.window_handles[0])
+
+    def _switch_to_window(self, window_index):
+        self.driver.switch_to_window(self.driver.window_handles[window_index])
+
+    def _post_msg(self, msg):
+        ActionChains(self.driver).send_keys(msg + '\n').perform()
+
+    @property
+    def _chat_log_val(self):
+        return self.driver.find_element_by_css_selector('#chat-log').get_property('value')
